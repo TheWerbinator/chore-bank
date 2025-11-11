3e97bc7444cec5f4a56cd10a00043758b3f9a48c
Shawn <shawnwerber@suu.edu>
2025-11-11 10:44:42 -0700

    Updated connection to env vars

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -10,8 +10,8 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -20,7 +20,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
+              cookieStore.set(name, value, options)
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -29,6 +29,6 @@ export async function createClient() {
           }
         },
       },
-    },
+    }
   );
 }
